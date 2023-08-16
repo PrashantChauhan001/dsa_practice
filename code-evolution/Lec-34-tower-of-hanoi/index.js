b83b43659ae03531412ecbe85161cd2146b43ce8
Prashant Chauhan <prashant.chauhan@globalvoxinc.com>
2023-08-16 15:37:53 +0530

    Tower of hanoi solved and understand y

--- code-evolution/Lec-34-tower-of-hanoi/index.js
+++ code-evolution/Lec-34-tower-of-hanoi/index.js
@@ -11,9 +11,20 @@ const minStep = (towerCount) => {
   return steps;
 };
 
+const stepLog = (towerCount, from = "A", to = "C", middle = "B") => {
+  if (towerCount === 1) {
+    console.log(`Move disk ${towerCount} from ${from} to ${to}`);
+    return;
+  }
+  stepLog(towerCount - 1, from, middle, to);
+  console.log(`Move disk ${towerCount} from ${from} to ${to}`);
+  stepLog(towerCount - 1, middle, to, from);
+};
+
 (async () => {
   const towerCount = await getNumberAnswer("Enter the tower count of hanoi");
   console.time("test");
   console.log(minStep(towerCount), "Min step required to solve the tower");
+  stepLog(towerCount), "Min step required to solve the tower";
   console.timeEnd("test");
 })();
